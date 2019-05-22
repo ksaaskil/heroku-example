20e0d2e01612755df663e58a283b7ba3affaf323
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-22 18:47:15 +0300

    Say big hello.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send({ message: "Hello from my app!" }));
+app.get("/", (req, res) => res.send({ message: "Big hello from my app!" }));
 
 // Important: port must be read from the environment for Heroku
 const port = process.env.PORT || 3000;
